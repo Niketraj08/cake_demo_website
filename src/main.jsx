ddbc37c2807aad17781b0d088340213a9599b919
Niketraj08 <niketrajkvs@gmail.com>
2026-03-26 15:31:43 +0530

    change the main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+// Importing the main App component and rendering it to the DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
