94f4baf787ee05358fe2c169f7994b4a0ed103d1
Niketraj08 <niketrajkvs@gmail.com>
2026-03-26 15:34:47 +0530

    index.html error solve

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,5 +21,5 @@ userSchema.pre('save', async function(next) {
 userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
-
+// Export the User model based on the userSchema
 module.exports = mongoose.model('User', userSchema);
